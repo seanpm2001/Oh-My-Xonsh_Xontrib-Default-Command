85ee8232fdb5ca6a3e74cdd0f4cebb4b735e2768
mattmc3 <mattmc3@gmail.com>
2021-11-13 13:07:33 -0500

    Bump version number to v0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-default-command',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     author='mattmc3',
     author_email='ohmyxonsh@gmail.com',
